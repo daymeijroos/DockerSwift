70a3194ac92027cc51f2a474fb06631bb57318d5
Michael Redig <mredig@gmail.com>
2024-11-25 23:25:47 -0600

    (refactor) validate start container endpoint

--- Tests/DockerSwiftTests/Mocks/StartContainerEndpoint.swift
+++ Tests/DockerSwiftTests/Mocks/StartContainerEndpoint.swift
@@ -1,3 +1,5 @@
+import AsyncHTTPClient
+import Foundation
 @testable import DockerSwift
 import NIO
 
@@ -6,5 +8,20 @@ extension StartContainerEndpoint: MockedResponseEndpoint {
 		[
 			.rawData(ByteBuffer())
 		]
+	}
+
+	public func validate(request: HTTPClientRequest) throws {
+		guard request.method == .POST else { throw DockerError.message("require post method") }
+		guard
+			let url = URL(string: request.url)
+		else { throw DockerError.message("invalid url") }
+
+		let components = url.pathComponents
+
+		guard
+			let containerIndex = components.firstIndex(of: "containers"),
+			(components.startIndex..<components.endIndex).contains(containerIndex + 2),
+			components[containerIndex + 2] == "start"
+		else { throw DockerError.message("invalid path for start: \(url)") }
 	}
 }
