b5a1906087a1e06c3ae25eda1bf43943fb77633a
Michael Redig <mredig@gmail.com>
2024-12-03 14:45:57 -0600

    (refactor) drop websocket dep

--- Package.swift
+++ Package.swift
@@ -10,12 +10,8 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/apple/swift-nio.git", from: "2.62.0"),
         .package(url: "https://github.com/swift-server/async-http-client.git", from: "1.19.0"),
-        // Container attach endpoint
-        .package(url: "https://github.com/vapor/websocket-kit.git", from: "2.6.1"),
         // Only used for parsing the multiple and inconsistent date formats returned by Docker
         .package(url: "https://github.com/marksands/BetterCodable.git", from: "0.4.0")
-        // Some Docker features receive or return TAR archives. Used by tests
-        //.package(url: "https://github.com/kayembi/Tarscape.git", .branch("main")),
     ],
     targets: [
         .target(
@@ -23,7 +19,6 @@ let package = Package(
             dependencies: [
                 .product(name: "NIO", package: "swift-nio"),
                 .product(name: "AsyncHTTPClient", package: "async-http-client"),
-                .product(name: "WebSocketKit", package: "websocket-kit"),
                 "BetterCodable",
 			],
 			swiftSettings: [
@@ -33,7 +28,6 @@ let package = Package(
             name: "DockerSwiftTests",
             dependencies: [
                 "DockerSwift",
-                //"Tarscape"
             ],
             resources: [
                 .copy("Assets")
